a04a63d2108aa1e01265ccd492433f71679d1426
Boris Glumpler <boris@shabushabu.eu>
2024-07-01 19:38:51 +0545

    docs: added config descriptions

--- config/uid.php
+++ config/uid.php
@@ -5,9 +5,65 @@ declare(strict_types=1);
 use Sqids\Sqids;
 
 return [
-    'alphabet' => env('UIDS_ALPHABET', Sqids::DEFAULT_ALPHABET),
-    'length' => (int) env('UIDS_LENGTH', 10),
-    'separator' => env('UIDS_SEPARATOR', '_'),
+    /*
+    |--------------------------------------------------------------------------
+    | Alphabet
+    |--------------------------------------------------------------------------
+    |
+    | It is recommended to set a custom alphabet here using the `uid:alphabet`
+    | command.
+    |
+    */
+
+    'alphabet' => env('UID_ALPHABET', Sqids::DEFAULT_ALPHABET),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Squids length
+    |--------------------------------------------------------------------------
+    |
+    | Please note that this is a minimum length and generated UIDs might be
+    | longer. The full minimum length of the generated UIDs will be the length
+    | of the prefix plus the length of the separator plus whatever you set here.
+    |
+    */
+
+    'length' => (int) env('UID_LENGTH', 10),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Separator
+    |--------------------------------------------------------------------------
+    |
+    | While you can use different characters here, please note that they need
+    | to be URL-safe. The underscore was chosen as it allows UIDs to be
+    | copied in full.
+    |
+    */
+
+    'separator' => env('UID_SEPARATOR', '_'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blocklist
+    |--------------------------------------------------------------------------
+    |
+    | A list of words you do not want to show up in your UIDs.
+    |
+    */
+
     'blocklist' => Sqids::DEFAULT_BLOCKLIST,
+
+    /*
+    |--------------------------------------------------------------------------
+    | UID prefixes
+    |--------------------------------------------------------------------------
+    |
+    | This setting completely takes over the morph map for your applications.
+    | You should specify all your models here (incl. pivot models) eg:
+    | ['usr' => \App\Models\User::class]
+    |
+    */
+
     'prefixes' => [],
 ];
